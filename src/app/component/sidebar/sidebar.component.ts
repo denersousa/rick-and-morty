1994347499d27e64b508ba266859fe1f89fbd082
Dener Sousa <90757341+denersousa@users.noreply.github.com>
2024-06-10 13:23:23 -0300

    Socumentação e ajuste do sidebar

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
+// Importações necessárias do Angular
 import { Router } from '@angular/router';
-import { LoginModel } from '../acess/models/login.model';
 import { UserModel } from '../acess/models/usuario';
 import { AuthService } from './../acess/services/auth.service';
 import { Component, OnInit } from '@angular/core';
@@ -7,55 +7,69 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrls: ['./sidebar.component.scss'] // Corrigido de 'styleUrl' para 'styleUrls'
 })
 export class SidebarComponent implements OnInit {
   public hideButton: boolean = false;
-  panelOpenState = false;
-  public dataUser: UserModel = {id: 0, name: '', newUserName: '', email: '', password: ''}
+  public panelOpenState: boolean = false;
+  public dataUser: UserModel = {id: 0, name: '', newUserName: '', email: '', password: ''};
 
   constructor(
     private authService: AuthService,
-    private router: Router){}
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    // Obtém os dados do usuário logado.
     this.dataUser = this.authService.getLoggedUser();
   }
 
+  // Método para abrir o menu lateral
   openNav() {
+    // Obtém referências aos elementos HTML.
     const sidebar = document.getElementById("mySidebar");
     const main = document.getElementById("main");
+
     if (sidebar && main) {
+      // Define a largura do menu lateral pe a margem esquerda do conteúdo principal.
       sidebar.style.width = "250px";
       main.style.marginLeft = "250px";
+
       this.hideButton = true;
     }
   }
 
+  // Método para fechar o menu lateral
   closeNav() {
+    // Obtém referências aos elementos HTML.
     const sidebar = document.getElementById("mySidebar");
     const main = document.getElementById("main");
+
     if (sidebar && main) {
+      // Define a largura do menu lateral e a margem esquerda do conteúdo principal
       sidebar.style.width = "0";
       main.style.marginLeft = "0";
       this.hideButton = false;
-
     }
   }
 
-  logoutGo(){
+  // Método para fazer logout
+  logoutGo() {
     this.authService.logout();
   }
 
-  listCharacterGo(){
-    this.router.navigate(['/lista-personagem'])
+  // Navega para a página de listagem de personagens
+  listCharacterGo() {
+    this.router.navigate(['/lista-personagem']);
   }
 
-  listaEPisodeGo(){
-    this.router.navigate(['/lista-episodios'])
+  // Navega para a página de listagem de episódios
+  listaEpisodeGo() {
+    this.router.navigate(['/lista-episodios']);
   }
 
-  homeGo(){
-    this.router.navigate(['/home'])
+  // Navega para a página inicial
+  homeGo() {
+    this.router.navigate(['/home']);
   }
 }
