a619364cd14214e4a02b0cc658837cd0056a4735
Dener Sousa <90757341+denersousa@users.noreply.github.com>
2024-06-10 12:34:16 -0300

    Documentação do component character e melhorias no codigo

--- src/app/component/character/services/character.service.ts
+++ src/app/component/character/services/character.service.ts
@@ -8,34 +8,42 @@ import { IdModel } from '../../shared/models/id.model';
   providedIn: 'root'
 })
 export class CharacterService {
-
-  private baseUrl = 'https://rickandmortyapi.com/api';
+  public baseUrl: string = 'https://rickandmortyapi.com/api';
 
   constructor(private http: HttpClient) { }
 
+  // Obtém um personagem pelo seu ID
   getCharacter(id: number): Observable<CharacterModel> {
-    return this.http.get<CharacterModel>(`${this.baseUrl}/character/${id}`)
+    return this.http.get<CharacterModel>(`${this.baseUrl}/character/${id}`);
   }
 
+  // Obtém todos os personagens
   getAllCharacters(): Observable<CharacterResponseModel> {
     return this.http.get<CharacterResponseModel>(`${this.baseUrl}/character`);
   }
 
+  // Obtém múltiplos personagens pelos seus IDs
   getMultipleCharacters(ids: IdModel[]): Observable<CharacterModel[]> {
+    // Filtra os IDs inválidos
     const idsFiltrados = ids.filter(id => id);
+    // Concatena os IDs em uma string separada por vírgulas
     const idsConcatenados = idsFiltrados.map(id => id.id).join(',');
+    // Retorna os personagens correspondentes aos IDs fornecidos
     return this.http.get<CharacterModel[]>(`${this.baseUrl}/character/${idsConcatenados}`);
   }
 
+  // Obtém um personagem pela sua URL
   getCharacterUrl(url: string): Observable<CharacterModel>{
     return this.http.get<CharacterModel>(`${url}`);
   }
 
+  // Obtém a próxima página de personagens
   getNextPage(url: string): Observable<CharacterResponseModel>{
-    return this.http.get<CharacterResponseModel>(`${url}`)
+    return this.http.get<CharacterResponseModel>(`${url}`);
   }
 
+  // Filtra os personagens pelo nome
   filter(data: string) : Observable<CharacterResponseModel>{
-    return this.http.get<CharacterResponseModel>(`${this.baseUrl}/character/?name=${data}`)
+    return this.http.get<CharacterResponseModel>(`${this.baseUrl}/character/?name=${data}`);
   }
 }
